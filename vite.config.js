0e75687a7297f462846f66b865785a026847ff37
jinwoo park <elvintern@gmail.com>
2023-06-11 17:06:38 +1200

    Add logo icons

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,13 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: './public/assets/hbb-logo.png',
+            src: './public/icon.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable',
           },
           {
-            src: './public/assets/hbb-logo.png',
+            src: './public/icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
