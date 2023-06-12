c170d4a0ba27d4e8a393a51d1bf3890024c4e4cc
jinwoo park <elvintern@gmail.com>
2023-06-12 13:28:07 +1200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.ico', 'robots.txt', 'img/**'],
       manifest: {
         src: 'manifest.webmanifest.js',
