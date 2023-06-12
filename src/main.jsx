5e7e556fbbe756d5c1620b264afaa6bee0de00d1
jinwoo park <elvintern@gmail.com>
2023-06-12 21:51:40 +1200

    Add confirming password function

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 if ('serviceWorker' in navigator) {
@@ -15,9 +15,8 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
