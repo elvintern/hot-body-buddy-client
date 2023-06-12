f809f857d2e06df68d3e0703b01394f5d21d1549
jinwoo park <elvintern@gmail.com>
2023-06-12 17:57:17 +1200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import imagemin from 'vite-plugin-imagemin';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
