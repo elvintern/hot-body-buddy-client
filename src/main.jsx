76ea0e2ae1e345a55c0f3532c84ed6d2cab25ac1
jinwoo park <elvintern@gmail.com>
2023-06-12 13:36:57 +1200

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../serviceWorker.js') // Path to your service worker file
+      .then((registration) => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed: ', error);
+      });
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
