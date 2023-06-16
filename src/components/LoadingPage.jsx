7887d8177a70f29d875f540588a62ffd7e515512
jinwoo park <elvintern@gmail.com>
2023-06-16 18:18:30 +1200

    Loading images

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
 import LoadingIcons from 'react-loading-icons';
+// import landscape1 from '../../public/assets/loadingImages/landscape-1.jpg';
+// import landscape2 from '../../public/assets/loadingImages/landscape-2.jpg';
+// import landscape3 from '../../public/assets/loadingImages/landscape-3.jpg';
+// import landscape4 from '../../public/assets/loadingImages/landscape-4.jpg';
+// import landscape5 from '../../public/assets/loadingImages/landscape-5.jpg';
+// import landscape6 from '../../public/assets/loadingImages/landscape-6.jpg';
+// import portrait1 from '../../public/assets/loadingImages/portrait-1.jpg';
+// import portrait2 from '../../public/assets/loadingImages/portrait-2.jpg';
 
 function LoadingPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -10,14 +18,10 @@ function LoadingPage() {
       let randNum = 1;
       if (window.innerWidth >= 992) {
         randNum = Math.floor(Math.random() * 6) + 1;
-        setLoadingImage(
-          `../../public/assets/loadingImages/landscape-${randNum}.jpg`
-        );
+        setLoadingImage(`assets/loadingImages/landscape-${randNum}.jpg`);
       } else {
         randNum = Math.floor(Math.random() * 2) + 1;
-        setLoadingImage(
-          `../../public/assets/loadingImages/portrait-${randNum}.jpg`
-        );
+        setLoadingImage(`assets/loadingImages/portrait-${randNum}.jpg`);
       }
       console.log(randNum);
 
