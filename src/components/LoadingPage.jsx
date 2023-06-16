32aa379f9ddc366192915e29e49314a35ba71b8d
jinwoo park <elvintern@gmail.com>
2023-06-16 19:06:02 +1200

    Loading images

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,31 +1,18 @@
 import { useState, useEffect } from 'react';
 import LoadingIcons from 'react-loading-icons';
-import landscape1 from '../../public/assets/loadingImages/landscape-1.jpg';
-import landscape2 from '../../public/assets/loadingImages/landscape-2.jpg';
-import landscape3 from '../../public/assets/loadingImages/landscape-3.jpg';
-import landscape4 from '../../public/assets/loadingImages/landscape-4.jpg';
-import landscape5 from '../../public/assets/loadingImages/landscape-5.jpg';
 import landscape6 from '../../public/assets/loadingImages/landscape-6.jpg';
 import portrait1 from '../../public/assets/loadingImages/portrait-1.jpg';
-import portrait2 from '../../public/assets/loadingImages/portrait-2.jpg';
 
 function LoadingPage() {
   const [isLoading, setIsLoading] = useState(true);
-  const [loadingImage, setLoadingImage] = useState(null);
+  const [isLandscape, setIsLandscape] = useState(true);
 
   useEffect(() => {
     const loadImage = async () => {
-      let randNum = 1;
       if (window.innerWidth >= 992) {
-        randNum = Math.floor(Math.random() * 6) + 1;
-        setLoadingImage(
-          `url(/assets/loadingImages/landscape-${randNum}.jpg) center/cover`
-        );
+        setIsLandscape(true);
       } else {
-        randNum = Math.floor(Math.random() * 2) + 1;
-        setLoadingImage(
-          `url(/assets/loadingImages/portrait-${randNum}.jpg) center/cover`
-        );
+        setIsLandscape(false);
       }
 
       const timer = setTimeout(() => {
@@ -44,7 +31,9 @@ function LoadingPage() {
         <div
           className="loading-image"
           style={{
-            background: loadingImage,
+            background: `url(/assets/loadingImages/${
+              isLandscape ? landscape6 : portrait1
+            }) center/cover`,
           }}
         >
           <LoadingIcons.BallTriangle className="loading-icon" width={'50rem'} />
